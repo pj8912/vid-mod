14d2e09569f314ae6b52418cb6b1cdaaa575de4c
jp <59218902+pj8912@users.noreply.github.com>
2023-03-15 13:01:01 +0530

    fixed input filename

--- webm_to_mp4.py
+++ webm_to_mp4.py
@@ -2,7 +2,7 @@ from moviepy.editor import *
 from moviepy.video.fx.all import *
 
 #video file
-clip = VideoFileClip("Screencast from 15-03-23 12:32:24 PM IST.webm")
+clip = VideoFileClip("FILENAME")
 
 #newname
 newname = "output.mp4"
